a61cc5c6b1ac467733d2bfd77ff6d9a8d8f10ad9
Eduardo García-Portugués <egarpor@users.noreply.github.com>
2025-03-01 17:13:39 +0100

    Add log_besselI_scaled_spline_* data

---
+++ R/zzz.R
@@ -0,0 +1,28 @@
+
+.onLoad <- function(libname = find.package("polykde"), pkgname = "polykde") {
+
+  # Assign global variables
+  env <- asNamespace(pkgname)
+  nus <- seq(0, 45, by = 5)
+  nus_char <- sprintf("%02d", nus)
+  for (nu_i in nus_char) {
+
+    assign(x = paste0("log_besselI_scaled_spline_", nu_i),
+           value =
+             splinefun(x = x_bessel,
+                       y = get(paste0("log_besselI_scaled_", nu_i, "_grid"),
+                               envir = env)),
+           pos = env)
+
+  }
+
+  # CRAN NOTE avoidance
+  if (getRversion() >= "2.15.1") {
+
+    utils::globalVariables(c("x_bessel",
+                             paste0("log_besselI_scaled_", nus_char, "_grid")))
+
+  }
+  invisible()
+
+}
