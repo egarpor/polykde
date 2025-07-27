a199f9068b49b05c25b396da6438936c6df66ee1
Eduardo García-Portugués <egarpor@users.noreply.github.com>
2025-07-27 17:13:26 +0200

    Refactor + complete ise_vmf_polysph()

--- tests/testthat/tests_mise.R
+++ tests/testthat/tests_mise.R
@@ -1,33 +1,26 @@
 
-test_that("exact_mise_vmf() does the job on computing exactly the MISE", {
+test_that("mise_vmf() does the job on computing exactly the MISE", {
 
   # Parameters
   M <- 1e4
-  n <- 2
-  d <- 1
-  m <- 1
-  mu <- rbind(c(1, 0))
-  kappa <- 5
-  prop <- 1
+  n <- 3
+  d <- rpois(1, lambda = 1) + 1
+  m <- 2
+  kappa <- rep(5, m)
+  mu <- r_unif_polysph(n = m, d = d)
+  prop <- rep(0.5, m)
   h <- 1
 
   # Sample X's and evaluate density
   set.seed(42)
   N1 <- 1e3
-  X <- lapply(seq_along(prop), function(m)
-    r_vmf_polysph(n = round(N1 * prop[m]), d = d, mu = mu[m, ],
-                  kappa = kappa))
-  X <- do.call(rbind, X)
-  f_X <- drop(kde_polysph(x = X, X = mu, d = d, h = 1 / sqrt(kappa),
-                          weights = prop))
+  X <- r_mvmf_polysph(n = N1, d = d, mu = mu, kappa = kappa, prop = prop)
+  f_X <- drop(d_mvmf_polysph(x = X, d = d, mu = mu, kappa = kappa, prop = prop))
 
   # Sample Y's to evaluate the kde
   N2 <- 1e3
   Y <- lapply(seq_len(N2), function(nj) {
-    Y_j <- lapply(seq_along(prop), function(m) {
-      r_vmf_polysph(n = round(n * prop[m]), d = d, mu = mu[m, ], kappa = kappa)
-    })
-    do.call(rbind, Y_j)
+    r_mvmf_polysph(n = n, d = d, mu = mu, kappa = kappa, prop = prop)
   })
 
   # Simulate sample and compute kde
@@ -38,52 +31,52 @@ test_that("exact_mise_vmf() does the job on computing exactly the MISE", {
   })
   kde_f_2 <- rowMeans(kde_f_2, na.rm = TRUE)
 
-  expect_equal(drop(exact_mise_vmf(h = h, n = n, mu = mu, kappa = kappa,
-                                     prop = prop, d = d, seed_psi = 42,
-                                     spline = TRUE)$mise),
+  expect_equal(drop(mise_vmf(h = h, n = n, mu = mu, kappa = kappa,
+                             prop = prop, d = d, seed_psi = 42,
+                             spline = TRUE)$mise),
                mean(kde_f_2 / f_X),
                tolerance = 1e-2)
 
 })
 
-test_that("exact_mise_vmf() is properly vectorized in h and n", {
+test_that("mise_vmf() is properly vectorized in h and n", {
 
   h <- c(0.1, 0.2)
   n <- c(10, 20)
   mu <- rbind(c(0, 1), c(1, 0))
   kappa <- c(5, 2)
   prop <- c(0.7, 0.3)
-  expect_equal(exact_mise_vmf(h = h, n = n, mu = mu, kappa = kappa,
-                              prop = prop, d = 1, seed_psi = 1, M_psi = 10,
-                              spline = TRUE)$mise,
-               c(exact_mise_vmf(h = h[1], n = n[1], mu = mu, kappa = kappa,
-                                prop = prop, d = 1, seed_psi = 1, M_psi = 10,
-                                spline = TRUE)$mise,
-                 exact_mise_vmf(h = h[2], n = n[2], mu = mu, kappa = kappa,
-                                prop = prop, d = 1, seed_psi = 1, M_psi = 10,
-                                spline = TRUE)$mise))
-  expect_equal(exact_mise_vmf(h = h, n = n[1], mu = mu, kappa = kappa,
-                              prop = prop, d = 1, seed_psi = 1, M_psi = 10,
-                              spline = TRUE)$mise,
-               c(exact_mise_vmf(h = h[1], n = n[1], mu = mu, kappa = kappa,
-                                prop = prop, d = 1, seed_psi = 1, M_psi = 10,
-                                spline = TRUE)$mise,
-                 exact_mise_vmf(h = h[2], n = n[1], mu = mu, kappa = kappa,
-                                prop = prop, d = 1, seed_psi = 1, M_psi = 10,
-                                spline = TRUE)$mise))
+  expect_equal(mise_vmf(h = h, n = n, mu = mu, kappa = kappa,
+                        prop = prop, d = 1, seed_psi = 1, M_psi = 10,
+                        spline = TRUE)$mise,
+               c(mise_vmf(h = h[1], n = n[1], mu = mu, kappa = kappa,
+                          prop = prop, d = 1, seed_psi = 1, M_psi = 10,
+                          spline = TRUE)$mise,
+                 mise_vmf(h = h[2], n = n[2], mu = mu, kappa = kappa,
+                          prop = prop, d = 1, seed_psi = 1, M_psi = 10,
+                          spline = TRUE)$mise))
+  expect_equal(mise_vmf(h = h, n = n[1], mu = mu, kappa = kappa,
+                        prop = prop, d = 1, seed_psi = 1, M_psi = 10,
+                        spline = TRUE)$mise,
+               c(mise_vmf(h = h[1], n = n[1], mu = mu, kappa = kappa,
+                          prop = prop, d = 1, seed_psi = 1, M_psi = 10,
+                          spline = TRUE)$mise,
+                 mise_vmf(h = h[2], n = n[1], mu = mu, kappa = kappa,
+                          prop = prop, d = 1, seed_psi = 1, M_psi = 10,
+                          spline = TRUE)$mise))
 
 })
 
-test_that("exact_mise_vmf_polysph() and exact_mise_vmf() equal on the sphere", {
+test_that("mise_vmf_polysph() and mise_vmf() equal on the sphere", {
 
   h <- 0.5
-  expect_equal(exact_mise_vmf(h = h, n = 100, mu = rbind(c(0, 1), c(1, 0)),
-                              kappa = c(5, 2), prop = c(0.7, 0.3), d = 1,
-                              seed_psi = 1, spline = TRUE),
-               exact_mise_vmf_polysph(h = h, n = 100,
-                                      mu = rbind(c(0, 1), c(1, 0)),
-                                      kappa = c(5, 2), prop = c(0.7, 0.3),
-                                      d = 1, seed_psi = 1, spline = TRUE))
+  expect_equal(mise_vmf(h = h, n = 100, mu = rbind(c(0, 1), c(1, 0)),
+                        kappa = c(5, 2), prop = c(0.7, 0.3), d = 1,
+                        seed_psi = 1, spline = TRUE),
+               mise_vmf_polysph(h = h, n = 100,
+                                mu = rbind(c(0, 1), c(1, 0)),
+                                kappa = c(5, 2), prop = c(0.7, 0.3),
+                                d = 1, seed_psi = 1, spline = TRUE))
 
 })
 
@@ -100,8 +93,8 @@ test_that("bw_mise_polysph() minimizes the MISE on the sphere", {
   n <- 10
   bw0 <- cbind(10^seq(log10(0.1), log10(5), l = 10))
   log1p_mise_bw0 <- sapply(bw0, function(h) {
-    log1p_mise_exact(log_h = log(h), n = n, mu = mu, kappa = kappa, prop = prop,
-                     d = d, seed_psi = 1, spline = TRUE)
+    log1p_mise(log_h = log(h), n = n, mu = mu, kappa = kappa, prop = prop,
+               d = d, seed_psi = 1, spline = TRUE)
   })
   bw_mise <- bw_mise_polysph(n = n, d = d, bw0 = bw0, mu = mu, kappa = kappa,
                              prop = prop, seed_psi = 1, spline = TRUE)
@@ -112,14 +105,53 @@ test_that("bw_mise_polysph() minimizes the MISE on the sphere", {
 
 })
 
-test_that("exact_ise_vmf() does the job on computing exactly the MISE", {
+test_that("ise_vmf_polysph() does the job on computing exactly the ISE", {
 
-  # TODO
+  # Parameters and sample
+  n <- 3
+  d <- rpois(1, lambda = 1) + 1
+  m <- 2
+  kappa <- rep(5, m)
+  mu <- r_unif_polysph(n = m, d = d)
+  prop <- rep(0.5, m)
+  h <- seq(0.1, 1, l = 10)
+  X <- r_mvmf_polysph(n = n, d = d, mu = mu, kappa = kappa, prop = prop)
+  expect_equal(
+    ise_vmf_polysph(X = X, d = d, h = h, mu = mu, kappa = kappa,
+                    prop = prop, spline = TRUE, exact = TRUE)$ise,
+    ise_vmf_polysph(X = X, d = d, h = h, mu = mu, kappa = kappa,
+                    prop = prop, seed_psi = 42, M_psi = 1e4,
+                    spline = TRUE, exact = FALSE)$ise,
+    tolerance = 5e-2
+  )
 
 })
 
 test_that("bw_ise_polysph() minimizes the ISE on the sphere", {
 
   # TODO
+
+  # r <- 1
+  # m <- rpois(1, 3) + 1
+  # d <- rpois(r, 3) + 1
+  # mu <- r_unif_polysph(n = m, d = d)
+  # kappa <- matrix(abs(rnorm(m * r, sd = 2)), nrow = m, ncol = r)
+  # prop <- runif(m)
+  # prop <- prop / sum(prop)
+  #
+  # n <- 10
+  # bw0 <- cbind(10^seq(log10(0.1), log10(5), l = 10))
+  # log1p_ise_bw0 <- sapply(bw0, function(h) {
+  #
+  #   log1p_ise_exact(log_h = log(h), n = n, mu = mu, kappa = kappa, prop = prop,
+  #                    d = d, seed_psi = 1, spline = TRUE)
+  # })
+  # bw_mise <- bw_ise_polysph(X = X, d = d, bw0 = bw0, mu = mu, kappa = kappa,
+  #                           prop = prop, seed_psi = 1, spline = TRUE)
+  # plot(bw0, log1p_ise_bw0, type = "o", xlab = "h", ylab = "log1p_mise",
+  #      xlim = range(c(bw0, bw_mise$bw)))
+  # points(bw_mise$bw, bw_mise$opt$minimum, col = "red", pch = 19)
+  # expect_lte(bw_mise$opt$minimum, min(log1p_mise_bw0))
+  #
 
 })
